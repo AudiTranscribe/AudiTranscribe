00fa3e41cf98cbc5d1730c05992111f77dd7f2ca
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-06-01 01:12:36 +0800

    Add Mel Spectrogram Code

--- src/main/java/site/overwrite/auditranscribe/spectrogram/spectral_representations/MelSpectrogram.java
+++ src/main/java/site/overwrite/auditranscribe/spectrogram/spectral_representations/MelSpectrogram.java
@@ -9,10 +9,91 @@
 
 package site.overwrite.auditranscribe.spectrogram.spectral_representations;
 
+import site.overwrite.auditranscribe.audio.WindowFunction;
+import site.overwrite.auditranscribe.misc.Complex;
+import site.overwrite.auditranscribe.utils.ArrayUtils;
+
 /**
  * Class that handles the Mel-based Spectrogram.
  */
 public class MelSpectrogram {
+    // Public methods
+
+    /**
+     * Compute a mel-scaled spectrogram.<br>
+     * To compute the mel-scaled spectrogram, its magnitude spectrogram <code>S</code> is first
+     * computed, then converted into power-based spectrogram, and then mapped onto the mel scale.
+     *
+     * @param x          Audio time series.
+     * @param sampleRate Sample rate of the audio signal.
+     * @param numFFT     Length of the FFT window.
+     * @param hopLength  Number of samples between successive frames.
+     * @return Mel spectrogram.
+     */
+    public static double[][] melSpectrogram(double[] x, double sampleRate, int numFFT, int hopLength) {
+        // Generate the STFT spectrogram
+        Complex[][] S = STFT.stft(x, numFFT, hopLength, WindowFunction.HANN_WINDOW);
+
+        // Convert the STFT spectrogram to power spectrogram
+        double[][] P = new double[S.length][S[0].length];
+        for (int i = 0; i < S.length; i++) {
+            for (int j = 0; j < S[0].length; j++) {
+                P[i][j] = Math.pow(S[i][j].abs(), 2);
+            }
+        }
+
+        // Continue processing in the other method
+        return melSpectrogram(P, sampleRate, numFFT);
+    }
+
+    /**
+     * Compute a mel-scaled spectrogram.<br>
+     * To compute the mel-scaled spectrogram, its magnitude spectrogram <code>S</code> is first
+     * computed, then converted into power-based spectrogram, and then mapped onto the mel scale.
+     *
+     * @param x          Audio time series.
+     * @param sampleRate Sample rate of the audio signal.
+     * @return Mel spectrogram.
+     */
+    public static double[][] melSpectrogram(double[] x, double sampleRate) {
+        return melSpectrogram(x, sampleRate, 2048, 512);
+    }
+
+    /**
+     * Compute a mel-scaled spectrogram.<br>
+     * A power spectrogram input P is mapped directly onto the mel basis by performing matrix
+     * multiplication.
+     *
+     * @param P          Power spectrogram.
+     * @param sampleRate Sample rate of the audio signal.
+     * @param numFFT     Length of the FFT window.
+     * @return Mel spectrogram.
+     */
+    public static double[][] melSpectrogram(double[][] P, double sampleRate, int numFFT) {
+        // Generate the mel filters
+        double[][] melFilters = melFilters(sampleRate, numFFT);
+
+        // Finally, create the spectrogram and return
+        return ArrayUtils.matmul(melFilters, P);
+    }
+
+    /**
+     * Compute a mel-scaled spectrogram.<br>
+     * A power spectrogram input P is mapped directly onto the mel basis by performing matrix
+     * multiplication.
+     *
+     * @param P          Power spectrogram.
+     * @param sampleRate Sample rate of the audio signal.
+     * @return Mel spectrogram.
+     */
+    public static double[][] melSpectrogram(double[][] P, double sampleRate) {
+        // Generate the mel filters
+        double[][] melFilters = melFilters(sampleRate, 2048);
+
+        // Finally, create the spectrogram and return
+        return ArrayUtils.matmul(melFilters, P);
+    }
+
     /**
      * Create a Mel filter-bank.<br>
      * This produces a linear transformation matrix to project FFT bins onto Mel-frequency bins.
@@ -83,6 +164,4 @@ public class MelSpectrogram {
     public static double[][] melFilters(double sampleRate, int numFFT) {
         return melFilters(sampleRate, numFFT, 128, 0, sampleRate / 2);
     }
-
-    // Todo: add actual mel spectrogram code
 }
