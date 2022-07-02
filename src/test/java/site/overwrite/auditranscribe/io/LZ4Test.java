0b1c01b4a78f8901ced3682483b962bf93084fc1
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-07-02 17:58:44 +0800

    Add LZ4 Task Test

--- src/test/java/site/overwrite/auditranscribe/io/LZ4Test.java
+++ src/test/java/site/overwrite/auditranscribe/io/LZ4Test.java
@@ -2,14 +2,16 @@
  * LZ4Test.java
  *
  * Created on 2022-05-04
- * Updated on 2022-05-10
+ * Updated on 2022-07-02
  *
  * Description: Test `LZ4.java`.
  */
 
 package site.overwrite.auditranscribe.io;
 
+import javafx.embed.swing.JFXPanel;
 import org.junit.jupiter.api.Test;
+import site.overwrite.auditranscribe.misc.CustomTask;
 
 import java.io.IOException;
 import java.util.HexFormat;
@@ -30,6 +32,32 @@ class LZ4Test {
 
         // Get the result of the compression
         byte[] result = LZ4.lz4Compress(HexFormat.of().parseHex(hexStr));
+
+        // Check if they are the same
+        assertEquals(HexFormat.of().formatHex(result), lz4HexStr);
+    }
+
+    @Test
+    void lz4CompressWithFakeTask() throws IOException {
+        // Start JavaFX toolkit
+        new JFXPanel();
+
+        // Define the hex string of the bytes to compress
+        String hexStr = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef" +
+                "f0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcde" +
+                "ef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcd" +
+                "def0123456789abcdef0123456789abcdef0123456789abcdef0123456789abc";
+
+        // Define the hex string of the supposed resulting bytes
+        String lz4HexStr = "8f0123456789abcdef0800058ff0123456789abcde0800051fef41000c1fde410000c056789abcdef0123456789abc";
+
+        // Get the result of the compression
+        byte[] result = LZ4.lz4Compress(HexFormat.of().parseHex(hexStr), new CustomTask<Void>() {
+            @Override
+            protected Void call() {
+                return null;
+            }
+        });
 
         // Check if they are the same
         assertEquals(HexFormat.of().formatHex(result), lz4HexStr);
