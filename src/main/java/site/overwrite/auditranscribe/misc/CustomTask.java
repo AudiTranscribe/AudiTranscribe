3c04696eae4c5a05300a0b0209c300cdc67f7286
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 11:46:33 +0800

    Update File Signatures For `misc`

--- src/main/java/site/overwrite/auditranscribe/misc/CustomTask.java
+++ src/main/java/site/overwrite/auditranscribe/misc/CustomTask.java
@@ -1,10 +1,19 @@
 /*
  * CustomTask.java
- *
- * Created on 2022-05-25
- * Updated on 2022-06-23
- *
  * Description: A custom task class where some protected methods are made public.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.misc;
