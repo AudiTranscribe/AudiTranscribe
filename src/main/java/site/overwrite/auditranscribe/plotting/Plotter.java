73c7908b80f871ce89402b3f9b0b1f4018c7ece5
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 11:57:26 +0800

    Update File Signatures For `plotting`

--- src/main/java/site/overwrite/auditranscribe/plotting/Plotter.java
+++ src/main/java/site/overwrite/auditranscribe/plotting/Plotter.java
@@ -1,10 +1,19 @@
 /*
  * Plotter.java
- *
- * Created on 2022-02-18
- * Updated on 2022-06-26
- *
  * Description: Class that contains plotting functions.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.plotting;
