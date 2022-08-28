bfd853ab697ed295ec7ba75e65be2c64938d529c
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 11:36:47 +0800

    Update File Signatures For Other `io` Classes
    ...and tests for `io` as well.

--- src/main/java/site/overwrite/auditranscribe/io/IOConstants.java
+++ src/main/java/site/overwrite/auditranscribe/io/IOConstants.java
@@ -1,10 +1,19 @@
 /*
  * IOConstants.java
- *
- * Created on 2022-06-06
- * Updated on 2022-07-02
- *
  * Description: Constants involved in the IO methods.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.io;
@@ -19,7 +28,9 @@ public final class IOConstants {
     public static final String SEPARATOR = File.separator;
 
     public static final String ROOT_ABSOLUTE_PATH = System.getProperty("user.dir");
-    public static final String TARGET_FOLDER_ABSOLUTE_PATH = IOMethods.joinPaths(ROOT_ABSOLUTE_PATH, "target", "classes");
+    public static final String TARGET_FOLDER_ABSOLUTE_PATH = IOMethods.joinPaths(
+            ROOT_ABSOLUTE_PATH, "target", "classes"
+    );
 
     public static final String USER_HOME_PATH = System.getProperty("user.home");
     public static final String TEMP_FOLDER_PATH = System.getProperty("java.io.tmpdir");
--- src/main/java/site/overwrite/auditranscribe/io/IOConverters.java
+++ src/main/java/site/overwrite/auditranscribe/io/IOConverters.java
@@ -1,10 +1,19 @@
 /*
  * IOConverters.java
- *
- * Created on 2022-05-01
- * Updated on 2022-06-28
- *
  * Description: Methods that converts Java objects/data into bytes for storage.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.io;
