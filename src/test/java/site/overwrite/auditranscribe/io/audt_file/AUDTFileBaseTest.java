9150db1ca0fcc03371253d5a312e058037220ca4
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 11:27:26 +0800

    Update File Signatures For `io.audt_file` Tests

--- src/test/java/site/overwrite/auditranscribe/io/audt_file/AUDTFileBaseTest.java
+++ src/test/java/site/overwrite/auditranscribe/io/audt_file/AUDTFileBaseTest.java
@@ -1,10 +1,19 @@
 /*
  * AUDTFileBaseTest.java
+ * Description: Base tests for the AUDT file reader and writer.
  *
- * Created on 2022-07-15
- * Updated on 2022-07-16
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
  *
- * Description: Basic tests for the AUDT file reader.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.io.audt_file;
--- src/test/java/site/overwrite/auditranscribe/io/audt_file/v401/data_encapsulators/AudioDataObject401Test.java
+++ src/test/java/site/overwrite/auditranscribe/io/audt_file/v401/data_encapsulators/AudioDataObject401Test.java
@@ -1,16 +1,26 @@
 /*
  * AudioDataObject401Test.java
- *
- * Created on 2022-07-02
- * Updated on 2022-07-11
- *
  * Description: Test `AudioDataObject401.java`.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.io.audt_file.v401.data_encapsulators;
 
 import org.junit.jupiter.api.Test;
 import site.overwrite.auditranscribe.io.audt_file.base.data_encapsulators.AudioDataObject;
+import site.overwrite.auditranscribe.io.audt_file.v401.data_encapsulators.AudioDataObject401;
 import site.overwrite.auditranscribe.utils.MathUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
