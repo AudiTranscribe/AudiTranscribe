a4c7047bdfdc282a5edef64905a1db850e57f54c
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 11:03:41 +0800

    Update File Signatures For `bpm_estimation` Package

--- src/main/java/site/overwrite/auditranscribe/bpm_estimation/BPMEstimator.java
+++ src/main/java/site/overwrite/auditranscribe/bpm_estimation/BPMEstimator.java
@@ -1,11 +1,20 @@
 /*
  * BPMEstimator.java
- *
- * Created on 2022-06-02
- * Updated on 2022-07-02
- *
  * Description: Class that handles all the methods to estimate the Beats Per Minute (BPM) of an
  *              audio signal.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.bpm_estimation;
