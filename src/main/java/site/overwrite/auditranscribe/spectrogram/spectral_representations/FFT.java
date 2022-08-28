0d2b653fd4a5321ef07ed590ea5cec98c2793108
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-28 12:10:43 +0800

    Update File Signatures For `spectrogram`

--- src/main/java/site/overwrite/auditranscribe/spectrogram/spectral_representations/FFT.java
+++ src/main/java/site/overwrite/auditranscribe/spectrogram/spectral_representations/FFT.java
@@ -1,14 +1,19 @@
 /*
  * FFT.java
- *
- * Created on 2022-02-12
- * Updated on 2022-07-17
- *
  * Description: Class that implements Fast Fourier Transform (FFT) algorithms.
  *
- * Partially adapted from
- * https://github.com/stefanGT44/AudioVisualizer-RealTime-Spectrogram/blob/master/src/app/FFT.java
- * and partially adapted from https://www.nayuki.io/page/free-small-fft-in-multiple-languages.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+ * Licence, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If
+ * not, see <https://www.gnu.org/licenses/>
+ *
+ * Copyright © AudiTranscribe Team
  */
 
 package site.overwrite.auditranscribe.spectrogram.spectral_representations;
