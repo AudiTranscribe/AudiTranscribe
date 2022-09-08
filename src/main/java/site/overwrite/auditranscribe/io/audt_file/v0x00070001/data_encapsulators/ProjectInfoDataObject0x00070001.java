950ef78a0d1de05729ebeaf65affc0e63fc30692
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-09-08 10:32:36 +0800

    Create Version `0x00070001` Data Encapsulators
    Also:
    - Added missing `projectName` attribute in the base `ProjectDataObject`'s `equals` and `hashCode` methods.
    - Moved one Q-transform test into a separate class for easier testing.

--- src/main/java/site/overwrite/auditranscribe/io/audt_file/v0x00070001/data_encapsulators/ProjectInfoDataObject0x00070001.java
+++ src/main/java/site/overwrite/auditranscribe/io/audt_file/v0x00070001/data_encapsulators/ProjectInfoDataObject0x00070001.java
@@ -1,5 +1,5 @@
 /*
- * ProjectInfoDataObject.java
+ * ProjectInfoDataObject0x00070001.java
  * Description: Data object that stores the project's info.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
@@ -16,7 +16,9 @@
  * Copyright © AudiTranscribe Team
  */
 
-package site.overwrite.auditranscribe.io.audt_file.base.data_encapsulators;
+package site.overwrite.auditranscribe.io.audt_file.v0x00070001.data_encapsulators;
+
+import site.overwrite.auditranscribe.io.audt_file.base.data_encapsulators.ProjectInfoDataObject;
 
 import java.util.Objects;
 
@@ -26,18 +28,31 @@ import java.util.Objects;
  * storing the project's info, whereas the <code>ProjectData</code> object stores all the different
  * data encapsulators.
  */
-public abstract class ProjectInfoDataObject extends AbstractAUDTDataObject {
-    // Constants
-    public static final int SECTION_ID = 4;
-
-    // Attributes
-    public String projectName;
-    public int musicKeyIndex;
-    public int timeSignatureIndex;
-    public double bpm;
-    public double offsetSeconds;
-    public double playbackVolume;
-    public int currTimeInMS;
+public class ProjectInfoDataObject0x00070001 extends ProjectInfoDataObject {
+    /**
+     * Initialization method for the project info data object.
+     *
+     * @param projectName        Name of the project.
+     * @param musicKeyIndex      The index of the music key in the dropdown menu shown in the
+     *                           application.
+     * @param timeSignatureIndex The index of the time signature in the dropdown menu shown in the application.
+     * @param bpm                Number of beats per minute.
+     * @param offsetSeconds      Number of seconds offset from the start of the audio.
+     * @param playbackVolume     Volume to play back at.
+     * @param currTimeInMS       Current playback time of the audio in <b>milliseconds</b>.
+     */
+    public ProjectInfoDataObject0x00070001(
+            String projectName, int musicKeyIndex, int timeSignatureIndex, double bpm, double offsetSeconds,
+            double playbackVolume, int currTimeInMS
+    ) {
+        this.projectName = projectName;
+        this.musicKeyIndex = musicKeyIndex;
+        this.timeSignatureIndex = timeSignatureIndex;
+        this.bpm = bpm;
+        this.offsetSeconds = offsetSeconds;
+        this.playbackVolume = playbackVolume;
+        this.currTimeInMS = currTimeInMS;
+    }
 
     // Overwritten methods
     @Override
@@ -61,5 +76,18 @@ public abstract class ProjectInfoDataObject extends AbstractAUDTDataObject {
         return Objects.hash(
                 projectName, musicKeyIndex, timeSignatureIndex, bpm, offsetSeconds, playbackVolume, currTimeInMS
         );
+    }
+
+    @Override
+    public int numBytesNeeded() {
+        return 4 +   // Section ID
+                (4 + projectName.getBytes().length) +  // String length + string bytes of project name
+                4 +  // Music key index
+                4 +  // Time signature index
+                8 +  // BPM
+                8 +  // Offset seconds
+                8 +  // Playback volume
+                4 +  // Current time in milliseconds
+                4;   // EOS delimiter
     }
 }
