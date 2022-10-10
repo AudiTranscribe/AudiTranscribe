0e272350e481e888f7ecad83342565e593a41dbb
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2022-10-10 22:12:09 +0800

    Update Repo
    - Add licence information to some Python files
    - Update workflows to search for `.java` files only

--- Other Files/Generate AUDT Test Files.py
+++ Other Files/Generate AUDT Test Files.py
@@ -1,5 +1,20 @@
 """
-Python program that generates the testing files for the specific file version.
+Generate AUDT Test Files.py
+
+Description: Python program that generates the testing files for the specific file version.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the
+GNU General Public Licence as published by the Free Software Foundation, either version 3 of the
+Licence, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public Licence for more details.
+
+You should have received a copy of the GNU General Public Licence along with this program. If not,
+see <https://www.gnu.org/licenses/>
+
+Copyright © AudiTranscribe Team
 """
 
 # IMPORTS
