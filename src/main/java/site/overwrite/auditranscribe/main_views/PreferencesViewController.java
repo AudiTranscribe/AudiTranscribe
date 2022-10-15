879a3025df5881581581f9ad71437ec4e8ea0897
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2022-10-15 11:10:07 +0800

    Move Some Classes Into `helpers`

--- src/main/java/site/overwrite/auditranscribe/main_views/PreferencesViewController.java
+++ src/main/java/site/overwrite/auditranscribe/main_views/PreferencesViewController.java
@@ -35,6 +35,7 @@ import site.overwrite.auditranscribe.audio.WindowFunction;
 import site.overwrite.auditranscribe.generic.ClassWithLogging;
 import site.overwrite.auditranscribe.io.IOMethods;
 import site.overwrite.auditranscribe.io.data_files.DataFiles;
+import site.overwrite.auditranscribe.main_views.helpers.ProjectIOHandlers;
 import site.overwrite.auditranscribe.misc.MyLogger;
 import site.overwrite.auditranscribe.misc.Popups;
 import site.overwrite.auditranscribe.misc.Theme;
