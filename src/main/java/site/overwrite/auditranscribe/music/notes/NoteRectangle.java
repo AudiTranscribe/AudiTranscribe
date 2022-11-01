fcb7adf167b8d5c2e329d9f4dfc25ffd11041e6d
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2022-11-01 23:04:19 +0800

    refactor: make `NoteRectangle` use new `TimeSignature`

--- src/main/java/site/overwrite/auditranscribe/music/notes/NoteRectangle.java
+++ src/main/java/site/overwrite/auditranscribe/music/notes/NoteRectangle.java
@@ -35,6 +35,7 @@ import site.overwrite.auditranscribe.generic.tuples.Pair;
 import site.overwrite.auditranscribe.io.data_files.DataFiles;
 import site.overwrite.auditranscribe.misc.MyLogger;
 import site.overwrite.auditranscribe.music.NoteUnit;
+import site.overwrite.auditranscribe.music.TimeSignature;
 import site.overwrite.auditranscribe.music.exceptions.NoteRectangleCollisionException;
 import site.overwrite.auditranscribe.plotting.PlottingHelpers;
 import site.overwrite.auditranscribe.utils.MusicUtils;
@@ -595,15 +596,15 @@ public class NoteRectangle extends StackPane {
      *
      * @param bpm           Beats per minute.
      * @param offset        Offset value.
-     * @param timeSignature Time signature string.
+     * @param timeSignature Time signature of the audio.
      */
-    public static void quantizeNotes(double bpm, double offset, String timeSignature) {
+    public static void quantizeNotes(double bpm, double offset, TimeSignature timeSignature) {
         // Only allow quantization if the playback is paused
         if (isPaused) {
             double pixelsPerSecond = spectrogramWidth / totalDuration;
 
             // Determine the note 'unit' we are working with
-            int noteUnit = MusicUtils.parseTimeSignature(timeSignature).value1();  // What "one beat" represents
+            int noteUnit = timeSignature.denominator.numericValue;  // What one beat represents
 
             // Get the number of seconds per beat
             double spb = 1. / bpm * 60.;  // spb = seconds per beat
