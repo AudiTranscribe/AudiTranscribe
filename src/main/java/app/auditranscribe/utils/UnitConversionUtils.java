61fbfeed3568b45c6fb658e8d7da212d5511e9ec
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-01-03 14:20:43 +0800

    Add Some Music Unit Conversion

--- src/main/java/app/auditranscribe/utils/UnitConversionUtils.java
+++ src/main/java/app/auditranscribe/utils/UnitConversionUtils.java
@@ -18,12 +18,110 @@
 
 package app.auditranscribe.utils;
 
+import app.auditranscribe.generic.exceptions.FormatException;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Unit conversion utilities.
  */
 public final class UnitConversionUtils {
     private UnitConversionUtils() {
         // Private constructor to signal this is a utility class
+    }
+
+    // Music unit conversion
+
+    /**
+     * Convert one or more note names to frequency.
+     *
+     * @param note Note string. Notes may be spelled out with optional accidentals or octave
+     *             numbers. The leading note name is case-insensitive. Sharps are indicated with
+     *             <code>#</code> or <code>♯</code>, flats may be indicated with <code>!</code>,
+     *             <code>b</code>, or <code>♭</code>.
+     * @return Frequency of that note.
+     */
+    public static double noteToFreq(String note) {
+        return noteNumberToFreq(noteToNoteNumber(note));
+    }
+
+    /**
+     * Converts a spelled note to the note number.<br>
+     * Note that the note number for C0 is 0 and subsequent note numbers are given by their offset
+     * from C0. For example, A4 has note number 57 as it is 57 notes away from C0.
+     *
+     * @param note Note string. Notes may be spelled out with optional accidentals or octave
+     *             numbers. The leading note name is case-insensitive. Sharps are indicated with
+     *             <code>#</code> or <code>♯</code>, flats may be indicated with <code>!</code>,
+     *             <code>b</code>, or <code>♭</code>.
+     * @return Note number for the given note.
+     * @throws FormatException the note format is incorrect.
+     * @implNote Double sharp and double flat are not currently supported.
+     */
+    public static int noteToNoteNumber(String note) {
+        // Define constants
+        final Map<String, Integer> PITCH_MAP = Map.of(
+                "C", 0,
+                "D", 2,
+                "E", 4,
+                "F", 5,
+                "G", 7,
+                "A", 9,
+                "B", 11
+        );
+
+        final Map<String, Integer> ACC_MAP = Map.of(
+                "#", 1,
+                "♯", 1,
+                "", 0,
+                "b", -1,
+                "!", -1,
+                "♭", -1
+        );
+
+        final Pattern NOTE_PATTERN = Pattern.compile(
+                "^(?<tonic>[A-Ga-g])(?<accidental>[#♯b!♭]*)(?<octave>[+-]?\\d+)?$"
+        );
+
+        // Attempt to match the `note` string to the pattern
+        Matcher matcher = NOTE_PATTERN.matcher(note);
+        if (!matcher.find()) {
+            throw new FormatException("Improper note format '" + note + "'");
+        }
+
+        // Get the matched groups from the note
+        String pitchChar = matcher.group("tonic").toUpperCase();
+        String offsetChars = matcher.group("accidental");
+        String octaveChar = matcher.group("octave");
+
+        // Now get the actual required numbers to form the note number
+        int offset = 0;
+        for (int i = 0; i < offsetChars.length(); i++) {
+            offset += ACC_MAP.get(Character.toString(offsetChars.charAt(i)));
+        }
+
+        int octave;
+        if (octaveChar == null) {
+            octave = 0;
+        } else {
+            octave = Integer.parseInt(octaveChar);
+        }
+
+        // Calculate note number and return
+        return 12 * octave + PITCH_MAP.get(pitchChar) + offset;
+    }
+
+    /**
+     * Converts the note number to a frequency.
+     *
+     * @param noteNumber The note number. Note that a note number of 0 means the key C0.
+     * @return Frequency of the note with that note number. The returned frequency assumes that the
+     * notes have been tuned to A440.
+     */
+    public static double noteNumberToFreq(int noteNumber) {
+        return 440 * Math.pow(2, (noteNumber - 57.) / 12);
     }
 
     // Audio unit conversion
--- src/test/java/app/auditranscribe/utils/UnitConversionUtilsTest.java
+++ src/test/java/app/auditranscribe/utils/UnitConversionUtilsTest.java
@@ -1,10 +1,72 @@
 package app.auditranscribe.utils;
 
+import app.auditranscribe.generic.exceptions.FormatException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnitConversionUtilsTest {
+    // Note unit conversion
+    @Test
+    void noteToFreq() {
+        assertEquals(440, UnitConversionUtils.noteToFreq("A4"), 0.001);
+        assertEquals(16.352, UnitConversionUtils.noteToFreq("C0"), 0.001);
+        assertEquals(15804.266, UnitConversionUtils.noteToFreq("B9"), 0.001);
+    }
+
+    @Test
+    void noteToNoteNumber() {
+        // Basic tests
+        assertEquals(0, UnitConversionUtils.noteToNoteNumber("c0"));
+        assertEquals(57, UnitConversionUtils.noteToNoteNumber("a4"));
+        assertEquals(60, UnitConversionUtils.noteToNoteNumber("c5"));
+        assertEquals(108, UnitConversionUtils.noteToNoteNumber("c9"));
+
+        // Full key tests
+        assertEquals(48, UnitConversionUtils.noteToNoteNumber("C4"));
+        assertEquals(49, UnitConversionUtils.noteToNoteNumber("C#4"));
+        assertEquals(50, UnitConversionUtils.noteToNoteNumber("D4"));
+        assertEquals(51, UnitConversionUtils.noteToNoteNumber("D#4"));
+        assertEquals(52, UnitConversionUtils.noteToNoteNumber("E4"));
+        assertEquals(53, UnitConversionUtils.noteToNoteNumber("F4"));
+        assertEquals(54, UnitConversionUtils.noteToNoteNumber("F#4"));
+        assertEquals(55, UnitConversionUtils.noteToNoteNumber("G4"));
+        assertEquals(56, UnitConversionUtils.noteToNoteNumber("G#4"));
+        assertEquals(57, UnitConversionUtils.noteToNoteNumber("A4"));
+        assertEquals(58, UnitConversionUtils.noteToNoteNumber("A#4"));
+        assertEquals(59, UnitConversionUtils.noteToNoteNumber("B4"));
+        assertEquals(60, UnitConversionUtils.noteToNoteNumber("C5"));
+
+        // Accidental tests
+        assertEquals(60, UnitConversionUtils.noteToNoteNumber("C5"));
+        assertEquals(61, UnitConversionUtils.noteToNoteNumber("C#5"));
+        assertEquals(61, UnitConversionUtils.noteToNoteNumber("C♯5"));
+        assertEquals(62, UnitConversionUtils.noteToNoteNumber("C##5"));
+        assertEquals(62, UnitConversionUtils.noteToNoteNumber("C♯♯5"));
+        assertEquals(59, UnitConversionUtils.noteToNoteNumber("Cb5"));
+        assertEquals(59, UnitConversionUtils.noteToNoteNumber("C!5"));
+        assertEquals(59, UnitConversionUtils.noteToNoteNumber("C♭5"));
+        assertEquals(58, UnitConversionUtils.noteToNoteNumber("Cbb5"));
+        assertEquals(58, UnitConversionUtils.noteToNoteNumber("C!!5"));
+        assertEquals(58, UnitConversionUtils.noteToNoteNumber("C♭♭5"));
+
+        // Weird 'no octave provided' tests
+        assertEquals(-2, UnitConversionUtils.noteToNoteNumber("C♭♭"));
+        assertEquals(2, UnitConversionUtils.noteToNoteNumber("C##"));
+
+        // Improper format tests
+        assertThrowsExactly(FormatException.class, () -> UnitConversionUtils.noteToNoteNumber("haha1"));
+        assertThrowsExactly(FormatException.class, () -> UnitConversionUtils.noteToNoteNumber("1C#"));
+        assertThrowsExactly(FormatException.class, () -> UnitConversionUtils.noteToNoteNumber("#C1"));
+    }
+
+    @Test
+    void noteNumberToFreq() {
+        assertEquals(440, UnitConversionUtils.noteNumberToFreq(57), 0.001);  // A4
+        assertEquals(16.352, UnitConversionUtils.noteNumberToFreq(0), 0.001);  // C0
+        assertEquals(15804.266, UnitConversionUtils.noteNumberToFreq(119), 0.001);  // B9
+    }
+
     // Audio unit conversion
     @Test
     void hzToOctaves() {
