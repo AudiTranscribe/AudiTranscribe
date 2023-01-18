9e6f7d3053812ca57b506698fbedeeabd8dca914
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-01-18 19:57:23 +0800

    Remove High Contrast Theme
    ...for now. Will add it back later.

--- src/main/java/app/auditranscribe/fxml/Theme.java
+++ src/main/java/app/auditranscribe/fxml/Theme.java
@@ -28,8 +28,7 @@ import app.auditranscribe.misc.ExcludeFromGeneratedCoverageReport;
 public enum Theme {
     // Enum values
     LIGHT("Light", "light"),
-    DARK("Dark", "dark"),
-    HIGH_CONTRAST("High Contrast", "high-contrast");
+    DARK("Dark", "dark");
 
     // Enum attributes
     private final String name;
